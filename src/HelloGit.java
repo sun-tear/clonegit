e13151e042db2df874300fc0d9fd2e8303f89ea9
dongxx <ssssss>
2017-03-01 10:42:42 +0800

    更新一个版本

---
+++ src/HelloGit.java
@@ -0,0 +1,12 @@
+
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("you`er so much welcome");
+		System.out.println("you`er so much welcome,version 1");
+	}
+
+}
